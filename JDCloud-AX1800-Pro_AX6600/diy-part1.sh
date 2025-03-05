74155260291b7b45593f8eb4fea93e8b7fc82638
Glory <lihonglin10086@163.com>
2025-03-05 22:40:53 +0800

    Update diy-part1.sh

--- JDCloud-AX1800-Pro_AX6600/diy-part1.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part1.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 本地脚本的基础目录
+SCRIPT_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 
-# Add a feed source
-git clone https://github.com/kenzok8/openwrt-packages package/kenzo
-git clone https://github.com/kenzok8/small package/small
+# 定义文件路径
+VERSION_FILE="${SCRIPT_BASE_DIR}/openwrt/include/version.mk"  # 脚本完整路径
+WIRELESS_FILE="${SCRIPT_BASE_DIR}/openwrt/target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh"
+MAC80211_FILE="${SCRIPT_BASE_DIR}/openwrt/package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"  # 脚本完整路径
+IMAGECONFIG_FILE="${SCRIPT_BASE_DIR}/openwrt/package/base-files/image-config.in"  # 脚本完整路径
+# 检查文件是否存在
+if [[ ! -f "$VERSION_FILE" ]]; then
+    echo "文件 $VERSION_FILE 不存在"
+    exit 1
+fi
+
+if [[ ! -f "$WIRELESS_FILE" ]]; then
+    echo "文件 $WIRELESS_FILE 不存在"
+    exit 1
+fi
+
+if [[ ! -f "$MAC80211_FILE" ]]; then
+    echo "文件 $MAC80211_FILE 不存在"
+    exit 1
+fi
+
+if [[ ! -f "$IMAGECONFIG_FILE" ]]; then
+    echo "文件 $IMAGECONFIG_FILE 不存在"
+    exit 1
+fi
+
+# 使用 sed 命令替换文本
+sed -i 's/LiBwrt/OpenWrt/g' "$VERSION_FILE"
+sed -i 's/LiBwrt/OpenWrt/g' "$WIRELESS_FILE"
+sed -i 's/LiBwrt/OpenWrt/g' "$MAC80211_FILE"
+sed -i 's/LiBwrt/OpenWrt/g' "$IMAGECONFIG_FILE"
+
+
+
+# 输出完成信息
+echo "文件 中所有的 'LIBwrt' 已经被替换为 'OpenWrt'"
